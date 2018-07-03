8524b9cbead735244ff9880b869b61b577234ce8
gzh-t184 <gzh-t184@DLT01037.dinpay.net>
2018-07-03 10:24:46 +0800

    打印数据工具类

---
+++ src/PrintData.java
@@ -0,0 +1,16 @@
+/**
+ * 打印数据工具类
+ * @author gzh-t184
+ *
+ */
+public class PrintData {
+	/**
+	 * 打印数组
+	 * @param a
+	 */
+	public void printArray(int[] a) {
+		for (int j = 0; j < a.length; j++) {
+			System.out.print(a[j] + " ");
+		}
+	}
+}
