76d4f6d07ebf7d674b6c30f6c67533c5f2b7ebec
gzh-t184 <gzh-t184@DLT01037.dinpay.net>
2018-07-03 10:41:52 +0800

    字符集改为UTF8

--- src/com/gzh/order/SelectOrder.java
+++ src/com/gzh/order/SelectOrder.java
@@ -1,52 +1,52 @@
 package com.gzh.order;
 /**
- * ѡ������
- * 1.ð������
- * ÿһ�ˣ������ķŵ����ұߡ�
- * ÿһ�ˣ��Ƚ�N�Σ�����N�Ρ�
- * 2.ѡ������
- * ÿһ�ˣ�����С�ķŵ�����ߡ�
- * ÿһ�ˣ��Ƚ�N�Σ�ֻ����һ�Ρ�����С�ĺ�����ߵĽ������ٶȵ�����Ҳ���ڽ��������������Ķ�һ�������ǵ��ٶȶ���N~2����Ϊ��2��forѭ��������ѡ���������ڽ���������һ�����Կ�һ�㡣
+ * 选择排序
+ * 1.冒泡排序
+ * 每一趟，把最大的放到最右边。
+ * 每一趟，比较N次，交换N次。
+ * 2.选择排序
+ * 每一趟，把最小的放到最左边。
+ * 每一趟，比较N次，只交换一次。把最小的和最左边的交换。速度的区别也在于交换次数，其他的都一样，他们的速度都是N~2，因为有2个for循环，但是选择排序由于交换次数少一点所以快一点。
  * @author gzh
  *
  */
 public class SelectOrder {
 
 	public static void main(String[] args) {
-		//��һ������
+		//有一个数组
 		int[] i = {10,9,8,7,6,5,4,3,2,1};
 		
-		//����֮ǰ
+		//排序之前
 		for (int j = 0; j < i.length; j++) {
 			System.out.print(i[j] + " ");
 		}
 		System.out.println();
 		
-		//����
-		selectOrder(i); //���򷽷�ֻ��Ҫ���򣬲���Ҫ�������ݣ���Ϊ�������������ã����Ե����򷽷����֮�������ֵ�Ѿ�������
+		//排序
+		selectOrder(i); //排序方法只需要排序，不需要返回数据，因为传参数组是引用，所以当排序方法完成之后数组的值已经被排序。
 		
-		//����֮��
+		//排序之后
 		for (int j = 0; j < i.length; j++) {
 			System.out.print(i[j] + " ");
 		}
 	}
 	
 	/**
-	 * ѡ������
-	 * @param i ����������
+	 * 选择排序
+	 * @param i 待排序数组
 	 */
 	public static void selectOrder(int[] i) {
-		for (int j = 0; j < i.length; j++) { //ÿһ��
-			int temp = j; //����һ����ʱֵ�����ڱ�����Сֵ
+		for (int j = 0; j < i.length; j++) { //每一趟
+			int temp = j; //都有一个临时值，用于保存最小值
 			for (int j2 = j+1; j2 < i.length; j2++) {
 				if (i[j2]<i[j]) {
 					temp = j2;
 				}
 			}
-			switchData(i,j,temp); //ÿһ�˱Ƚ����֮�󣬰���Сֵ�ŵ������
+			switchData(i,j,temp); //每一趟比较完毕之后，把最小值放到最左边
 			
 			//
-			for (int j1 = 0; j1 < i.length; j1++) { //ÿһ�����֮�󣬴�ӡֵ
+			for (int j1 = 0; j1 < i.length; j1++) { //每一趟完毕之后，打印值
 				System.out.print(i[j1] + " ");
 			}
 			System.out.println();
@@ -54,15 +54,15 @@ public class SelectOrder {
 	}
 	
 	/**
-	 * ��������
-	 * @param i ����
+	 * 交换数据
+	 * @param i 数组
 	 * @param j 
 	 * @param j2
 	 */
 	public static void switchData(int[] i,int j, int j2) {
 		int temp = 0;
-		temp = i[j]; //����
-		i[j]=i[j2]; //����
+		temp = i[j]; //保存
+		i[j]=i[j2]; //交换
 		i[j2]=temp;
 	}
 
