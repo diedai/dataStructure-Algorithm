b6221e5a2d66627e0a4a8474e7f8144a870e7f23
gongzhihao <gongzhihao@192.168.1.101>
2018-08-24 21:47:00 +0800

    归并排序伪代码

--- src/com/gzh/order/MergeOrder.java
+++ src/com/gzh/order/MergeOrder.java
@@ -9,8 +9,33 @@ package com.gzh.order;
 public class MergeOrder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		recMergeOrder();
+	}
+	
+	
 
+	private static void recMergeOrder() {
+		//剩下一个数据结束递归调用
+		
+		//否则
+		//递归调用左边
+		//递归调用右边
+		
+		//归并排序
+		mergeOrder();
+	}
+
+
+
+	private static void mergeOrder() {
+		//复制A数组里大的数据
+		
+		//否则，复制B数组里大的数据
+		
+		
+		//复制剩下的A数组里的数据
+		
+		//复制剩下的B数组里的数据
 	}
 
 }
