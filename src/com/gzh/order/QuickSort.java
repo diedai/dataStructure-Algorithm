a20a54cfcc914cf7a225aa6a0d62100b57de5f18
gongzhihao <gongzhihao@192.168.1.101>
2018-09-28 22:30:19 +0800

    划分算法

--- src/com/gzh/order/QuickSort.java
+++ src/com/gzh/order/QuickSort.java
@@ -1,10 +1,55 @@
 package com.gzh.order;
 
+import com.gzh.util.PrintData;
+import com.gzh.util.SwapData;
+
+/**
+ * 快速排序
+ * <pre>
+ * 思想
+ * 1.
+ * 2.
+ * @author gongzhihao
+ * @date 2018年9月28日 下午10:29:00
+ * </pre>
+ */
 public class QuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PrintData printData = new PrintData();
+		
+		//划分算法
+		//数据
+		int[] a= {10,9,8,7,6,5,4,3,2,1};
+		
+		//排序数据
+		int left = 0, right = a.length ;
+		int leftP = left-1, rightP = right;
+		int pivot = 5;
+		while(true) {
+			//从最左边开始遍历数据，直到找到一个比基准值大的数据
+			while(leftP<right && a[++leftP]<pivot) {
+				
+			}
+			
+			//从最右边开始遍历数据，直到找到一个比基准值小的数据
+			while(rightP>left && a[--rightP]>pivot) {
+				
+			}
+			
+			if(leftP >= rightP) { //结束掉外层while的标志条件要放在内层while和交换数据的代码之间，因为内层while循环的指针会自增和自减，自增和自减到最后一个数据的时候，就是结束条件，这个时候要结束外层while循环，因为数据已经局部有序了，否则的话，还会交换一次数据，导致数据又乱序了(例如，假设把结束条件放在while循环的条件判断语句，就会多比较最后一次)。
+				break;
+			}else {
+				//交换数据。交换大的数据和小的数据，把它们放到基准值的右边和左边
+				SwapData swapData = new SwapData();
+				swapData.swapData(a, leftP, rightP);
+				
+				printData.printArray(a);
+			}
+		}
+		
+		//排序之后
+	    printData.printArray(a);
 	}
 
 }
